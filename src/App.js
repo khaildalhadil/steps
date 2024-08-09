fc45c55c0a2f310ce48f2767332096f025099ab3
Khaild <khaild.alhadi2020@gmail.com>
2024-08-09 18:12:16 +0400

    finash steps app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import {useState} from 'react';
 
-function App() {
+const messages = [
+  'تعلم رياكت ⚛️',
+  'قدم على وظيفة 💼',
+  'عيش انجوي  🤑',
+];
+
+
+export default function App() {
+
+  const [step, setCount] = useState(1);
+
+  const btnStyle = {background: "#7950f2", color: '#fff'}
+  
+  const previous = ()=> {
+    if (step <= 1) return
+    setCount(step-1)
+  }
+  
+  const next = ()=> {
+    if (step >= 3) return
+    setCount(step + 1)
+  } 
+  
+  console.log(step)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps" >
+
+      <div className='numbers' >
+        <div className={step >= 1? 'active': ''} >1</div>
+        <div className={step >= 2? 'active': ''} >2</div>
+        <div className={step >= 3? 'active': ''} >3</div>
+      </div>
+
+      <p className="message" >الخطوه {step}: {messages[step - 1]}</p>
+
+      <div className="buttons" >
+        <button style={btnStyle} onClick={previous} >Previous</button>
+        <button style={btnStyle} onClick={next} >Next</button>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
